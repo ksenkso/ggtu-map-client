0d2ba7089105f45fe97dcf761633d0826a66805a
ksenkso <yazunya@gmail.com>
2019-03-10 11:09:32 +0300

    main layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,56 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+        <div ref="mapContainer" class="map-container"></div>
+        <SupportContainer></SupportContainer>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+    import {Scene} from 'ggtu-map';
+    import 'ggtu-map/dist/main.css';
+    import SupportContainer from "@/components/SupportContainer";
+    let scene;
+    export default {
+        name: 'home',
+        components: {SupportContainer,},
+        data() {
+            return {
+                locations: {
+                    type: Array,
+                    default: []
+                },
+                currentLocation: {},
+                currentBuilding: {}
+            }
+        },
+        mounted() {
+            scene = new Scene(this.$refs.mapContainer);
+            this.$api.locations.getRoot()
+                .then(location => {
+                    scene.setLocation(location)
+                        .then(() => {
+                            this.currentLocation = location;
+                        })
+                })
+        },
+        provide: {
+            /**
+             *
+             * @return {IScene}
+             */
+            getScene() {
+                return scene;
+            }
+        }
+    }
 </script>
+<style lang="sass">
+.home
+    position: relative
+    width: 100%
+    height: 100%
+    .map-container
+        position: absolute
+        width: 100%
+        height: 100%
+</style>
