6c2f9d87ad860a8ebfc1a2dda071f4cc069ca4b8
ksenkso <yazunya@gmail.com>
2019-03-10 11:09:08 +0300

    bootstrap

--- src/main.js
+++ src/main.js
@@ -2,10 +2,23 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './registerServiceWorker'
+import BootstrapVue from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import api from './api';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(BootstrapVue);
+api.authenticate('public', 'public')
+    .then(() => {
+        /**
+         * @type ApiClient
+         */
+        Vue.prototype.$api = api;
+        new Vue({
+            router,
+            render: h => h(App)
+        }).$mount('#app');
+    });
 
-new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
+
