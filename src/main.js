83084e2a63db376afcb4103656fe142c52f3ee1c
ksenkso <yazunya@gmail.com>
2019-05-12 08:42:16 +0300

    remove bootstrap

--- src/main.js
+++ src/main.js
@@ -2,13 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './registerServiceWorker'
-import BootstrapVue from 'bootstrap-vue';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
 import api from './api';
 
 Vue.config.productionTip = false;
-Vue.use(BootstrapVue);
 api.authenticate('public', 'public')
     .then(() => {
         /**
